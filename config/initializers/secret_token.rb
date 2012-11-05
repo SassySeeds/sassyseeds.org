0498fca3fec756012bcf1aa87a0e386488b6878b
matt <matthewcopeland1@gmail.com>
2012-11-04 23:42:58 -0600

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sassyseedsorg::Application.config.secret_token = 'ddd698268a3e70868e7f71289de4e14d6f81b32ba7574d53bd93e9cb03eda64609db52cf632c76b09a9cb5e01f013170e6772b59192fc932cd76ba2ab6cb0791'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Sassyseedsorg::Application.routes.draw do
+  root :to => "home#index"
+end
